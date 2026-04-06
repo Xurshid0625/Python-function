2a876c185d6e0dee21f9b6e3dbee1991f36e5b67
Xurshid Abdumannonov <abdumannonovxurshid0625@gmail.com>
2026-04-06 13:16:13 +0500

    function global

--- python.py
+++ python.py
@@ -1,11 +1,26 @@
-def work():
+# def work():
     
-    print("Python is working!")
+#     print("Python is working!")
     
-work()
+# work()
 
-def job():
+# def job():
     
-    print("Python is working!")
+#     print("Python is working!")
     
-job()
+# job()
+
+# def text(age,name):
+    
+#     print(age,name)
+    
+    
+# text(25,"John")
+# a = "Hello, World!"
+# def job(name,year):
+    
+#     print(f"mijozname:{name},\n mijozage:{2026 - year}\n mujozni yili:{year}\n")
+#     print(a)
+
+# job("John",1990)
+# job("Alice",1985) 
